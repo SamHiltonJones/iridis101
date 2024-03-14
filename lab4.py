b12478b91fd1a1fa6e7c26f00e762e81f50d417e
SamHiltonJones <sam.hiltonjones@gmail.com>
2024-03-14 11:21:57 +0000

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -44,6 +44,21 @@ def save_joint_plot(data, title, folder, filename, layer_sizes):
     plt.legend()
     plt.savefig(f"results/{folder}/{filename}.png")
     plt.close()
+    
+def save_combined_joint_plot(train_data, val_data, title, folder, filename, layer_sizes):
+    plt.figure()
+    num_layers = len(layer_sizes)
+
+    for i in range(num_layers):
+        plt.plot(train_data[i], label=f'Train Layer {layer_sizes[i]}', linestyle='-', marker='o')
+
+    for i in range(num_layers):
+        plt.plot(val_data[i], label=f'Val Layer {layer_sizes[i]}', linestyle='--', marker='x')
+
+    plt.title(title)
+    plt.legend()
+    plt.savefig(f"results/{folder}/{filename}.png")
+    plt.close()
 
 if __name__ == '__main__':
     # Setup argument parser
@@ -78,59 +93,53 @@ if __name__ == '__main__':
     trainloader = DataLoader(trainset, batch_size=args.batch_size, shuffle=True)
     testloader = DataLoader(testset, batch_size=args.batch_size, shuffle=True)
 
-    all_train_accuracies = []
-    all_test_accuracies = []
-    all_train_losses = []
-    all_test_losses = []
+    all_train_acc = []
+    all_val_acc = []
+    all_train_loss = []
+    all_val_loss = []
 
     for size in args.layer_sizes:
         model = BaselineModel(784, size, 10).to(device)
         loss_function = nn.CrossEntropyLoss()
         optimizer = optim.Adam(model.parameters())
 
-        train_accuracies = []
-        test_accuracies = []
-        train_losses = []
-        test_losses = []
-
-        for epoch in range(args.epochs):
-            print(f'Layer Size: {size}, Epoch: {epoch +1}')
-            trial = Trial(model, optimizer, loss_function, metrics=['accuracy', 'loss']).to(device)
-            trial.with_generators(trainloader, test_generator=testloader)
-            trial.run(epochs=1)
-            
-            train_results = trial.evaluate(data_key=torchbearer.TRAIN_DATA)
-            test_results = trial.evaluate(data_key=torchbearer.TEST_DATA)
-            
-            train_accuracies.append(train_results['train_acc'])
-            test_accuracies.append(test_results['test_acc'])
-            train_losses.append(train_results['train_loss'])
-            test_losses.append(test_results['test_loss'])
+        trial = Trial(model, optimizer, loss_function, metrics=['accuracy', 'loss']).to(device)
+        trial.with_generators(trainloader, val_generator=testloader)
+        
+        history = trial.run(epochs=2)
+        results = trial.evaluate(data_key=torchbearer.TEST_DATA)
+        
+        
+        train_loss = [epoch['loss'] for epoch in history]
+        val_loss = [epoch['val_loss'] for epoch in history]
+        
+        train_acc = [epoch['acc'] for epoch in history]
+        val_acc = [epoch['val_acc'] for epoch in history]
             
         torch.save(model.state_dict(), f'results/model_weights_{size}.pth')
 
-        all_train_accuracies.append(train_accuracies)
-        all_test_accuracies.append(test_accuracies)
-        all_train_losses.append(train_losses)
-        all_test_losses.append(test_losses)
+        all_train_acc.append(train_acc)
+        all_val_acc.append(val_acc)
+        all_train_loss.append(train_loss)
+        all_val_loss.append(val_loss)
 
     for i, size in enumerate(args.layer_sizes):
-        save_combined_plot(all_train_accuracies[i], all_test_accuracies[i], f"Accuracy for Layer Size {size}", "individual", f"{size}_accuracy")
-        save_combined_plot(all_train_losses[i], all_test_losses[i], f"Loss for Layer Size {size}", "individual", f"{size}_loss")
+        save_combined_plot(all_train_acc[i], all_val_acc[i], f"Train vs Validation Accuracy for Layer Size {size}", "individual", f"{size}_accuracy")
+        save_combined_plot(all_train_loss[i], all_val_loss[i], f"Train vs Validation Loss for Layer Size {size}", "individual", f"{size}_loss")
 
-    save_joint_plot(all_train_accuracies, "Training Accuracy for All Layer Sizes", "train", "all_accuracy", args.layer_sizes)
-    save_joint_plot(all_train_losses, "Training Loss for All Layer Sizes", "train", "all_loss", args.layer_sizes)
-    save_joint_plot(all_test_accuracies, "Testing Accuracy for All Layer Sizes", "test", "all_accuracy", args.layer_sizes)
-    save_joint_plot(all_test_losses, "Testing Loss for All Layer Sizes", "test", "all_loss", args.layer_sizes)
-
-    save_joint_plot(all_train_accuracies + all_test_accuracies, "Accuracy for All Layer Sizes", "joint", "all_accuracy", args.layer_sizes + args.layer_sizes)
-    save_joint_plot(all_train_losses + all_test_losses, "Loss for All Layer Sizes", "joint", "all_loss", args.layer_sizes + args.layer_sizes)
-
+    save_joint_plot(all_train_acc, "Training Accuracy for All Layer Sizes", "train", "all_train_accuracy", args.layer_sizes)
+    save_joint_plot(all_train_loss, "Training Loss for All Layer Sizes", "train", "all_train_loss", args.layer_sizes)
+    save_joint_plot(all_val_acc, "Validation Accuracy for All Layer Sizes", "test", "all_val_accuracy", args.layer_sizes)
+    save_joint_plot(all_val_loss, "Validation Loss for All Layer Sizes", "test", "all_val_loss", args.layer_sizes)
+    
+    save_combined_joint_plot(all_train_acc, all_val_acc, "Train and Validation Accuracy for All Layer Sizes", "joint", "all_accuracy", args.layer_sizes)
+    save_combined_joint_plot(all_train_loss, all_val_loss, "Train and Validation Loss for All Layer Sizes", "joint", "all_loss", args.layer_sizes)
+    
     results_data = {
-        "train_accuracies": {str(size): accuracies for size, accuracies in zip(args.layer_sizes, all_train_accuracies)},
-        "test_accuracies": {str(size): accuracies for size, accuracies in zip(args.layer_sizes, all_test_accuracies)},
-        "train_losses": {str(size): losses for size, losses in zip(args.layer_sizes, all_train_losses)},
-        "test_losses": {str(size): losses for size, losses in zip(args.layer_sizes, all_test_losses)}
+        "all_train_acc": {str(size): accuracies for size, accuracies in zip(args.layer_sizes, all_train_acc)},
+        "all_val_acc": {str(size): accuracies for size, accuracies in zip(args.layer_sizes, all_val_acc)},
+        "all_train_loss": {str(size): losses for size, losses in zip(args.layer_sizes, all_train_loss)},
+        "all_val_loss": {str(size): losses for size, losses in zip(args.layer_sizes, all_val_loss)}
     }
 
     with open('results/results_data.json', 'w') as json_file:
